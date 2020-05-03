d983a582d1099e281845501887976986cfe28648
dima <dimitritkach@gmail.com>
2020-05-03 18:55:14 +0200

    Added local settings import

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -147,3 +147,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimitritkach@gmail.com'
 EMAIL_HOST_PASSWORD = 'Tap|\|an'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
